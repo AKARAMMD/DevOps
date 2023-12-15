936f2d7e9d154c17d3c33b4a6dd7ee6ce5b14569
Akaram Siddiqui <akaram.siddiqui@amconsultcorp.com>
2023-12-15 12:31:20 +0530

    Fixed issue

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
 	public static void main(String [] args){
 System.out.println("Welcome to get online training!");
-System.out.println("This is new line")
+System.out.println("This is new line");
 
 	}
 
